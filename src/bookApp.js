1d88c59720b8a4363eeb0b8ee2cf6eb31ffddf99
Eric Rubio <eric.rubio.services@gmail.com>
2019-08-25 14:56:00 -0500

    first commit

---
+++ src/bookApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BookApp() {
+  return (
+    <main className='bookApp'>
+        <h1>Super Hello There!</h1>
+      <p>Hello there!</p>
+    </main>
+  );
+}
+
+export default BookApp;
